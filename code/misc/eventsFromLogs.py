a3f83d4755db9b22af243caa09ee48b9808b3c49
Sebastian Dresbach <sebastiandresbach@sebastians-mbp.wired.unimaas.local>
2022-08-10 17:08:19 +0200

    add logfiles of sub-01_ses-01

--- code/misc/eventsFromLogs.py
+++ code/misc/eventsFromLogs.py
@@ -8,113 +8,55 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # define root dir
-root = '/Users/sebastiandresbach/git'
+root = '/Users/sebastiandresbach/git/neurovascularCouplingVASO'
 # define subjects to work on
 subs = ['sub-01']
 
 
 for sub in subs:
     # get all runs of all sessions
-    logFiles = sorted(glob.glob(f'/{root}/code/stimulation/{sub}/ses-*/func/{sub}_ses-*_run-0*_neurovascularCoupling.log'))
+    logFiles = sorted(glob.glob(f'{root}/code/stimulation/{sub}/ses-*/{sub}_ses-*_run-0*_neurovascularCoupling.log'))
 
     for logFile in logFiles:
         # get basename of current run
-        base = os.path.basename(logFile).rsplit('.', 2)[0][:-4]
+        base = os.path.basename(logFile).rsplit('.', 2)[0]
         # get logfile
-        logFile = pd.read_csv(f'{logFile}'
+        data = pd.read_csv(f'{logFile}'
                                 ,usecols=[0])
 
         # Because the column definition will get hickups if empty colums are
         # present, we find line with first trigger to then load the file anew,
         # starting with that line
-        for index, row in logFile.iterrows():
-            if re.search('Keypress: 5', str(row)):
-                firstVolRow = index
+        for index, row in data.iterrows():
+            if re.search('StartOfRun', str(row)):
+                firstVolRow = index+1
                 break
         # define column names
         ColNames = ['startTime', 'type', 'event']
+
         # load logfile again, starting with first trigger
+        data = pd.read_csv(f'{logFile}', sep = '\t', skiprows=firstVolRow, names = ColNames)
 
-        logFile = pd.read_csv(f'{logFile}', sep = '\t', skiprows=firstVolRow, names = ColNames)
+        # initiate lists
+        stimStart = []
+        stimStop = []
 
-#         # initiate lists
-#         stimStart = []
-#         stimStop = []
-#
-#
-#
-#
-#         # loop over lines and fine stimulation start and stop times
-#         for index, row in logFile.iterrows():
-#         # because the block/event logfiles have different codes at the start/end
-#         # of stimulation, we have to code them differently.
-#             if 'block' in run:
-#                 if re.search('stimulation started', logFile['event'][index]):
-#                     stimStart.append(logFile['startTime'][index])
-#                 if re.search('stimulation stopped', logFile['event'][index]):
-#                     stimStop.append(logFile['startTime'][index])
-#             if 'event' in run:
-#                 if re.search('visual stimulation started', logFile['event'][index]):
-#                     stimStart.append(logFile['startTime'][index])
-#                 if re.search('visual stimulation stopped', logFile['event'][index]):
-#                     stimStop.append(logFile['startTime'][index])
-#         # convert lists to arrays and compute stimulation durations
-#         durs = np.asarray(stimStop) - np.asarray(stimStart)
-#
-#         # make dataframe and save as text file
-#         design = pd.DataFrame({'startTime': stimStart, 'duration': durs, 'mod' : np.ones(len(durs))})
-#         np.savetxt(f'{root}/derivatives/{sub}/{ses}/events/{base}.txt', design.values, fmt='%1.2f')
-#
-#
-#         # make design files to compare short vs long ITIs
-#         if 'blockStim' in base:
-#             continue
-#
-#         meanDur = np.mean(durs)
-#         # the first ITI is due the time between start of run and first stimulus
-#         ITIs = [19]
-#         for i, trial in enumerate(stimStart[:-1]):
-#             tmp = (stimStart[i+1]-trial)-meanDur
-#             ITIs.append(tmp)
-#
-#         # get max ITI
-#         ITImax = np.amax(ITIs[1:]) # exclude first, long ITI
-#         # get min ITI
-#         ITImin = np.amin(ITIs)
-#         # get middle ITI
-#         middleITI = ((ITImax-ITImin)/2)+ITImin
-#         medianITI = np.median(ITIs)
-#
-#         usedTrials = []
-#
-#         for type in ['longITI', 'shortITI']:
-#             # print(type)
-#             splitStarts = []
-#             splitDurs = []
-#
-#             for i, ITI in enumerate(ITIs):
-#                 if type == 'longITI' and ITI > medianITI:
-#                     # print(ITI)
-#                     splitStarts.append(stimStart[i])
-#                     splitDurs.append(durs[i])
-#
-#                     if i in usedTrials:
-#                         print('double assignment of trial')
-#                     usedTrials.append(i)
-#
-#                 if type == 'shortITI' and ITI < medianITI:
-#                     # print(ITI)
-#                     splitStarts.append(stimStart[i])
-#                     splitDurs.append(durs[i])
-#                     if i in usedTrials:
-#                         print('double assignment of trial')
-#
-#                     usedTrials.append(i)
-#
-#             design = pd.DataFrame({'startTime': splitStarts, 'duration': splitDurs, 'mod' : np.ones(len(splitDurs))})
-#             np.savetxt(f'{root}/derivatives/{sub}/{ses}/events/{base}_{type}.txt', design.values, fmt='%1.2f')
-#
-#
+        # loop over lines and fine stimulation start and stop times
+        for index, row in data.iterrows():
+            if not data['event'][index] != data['event'][index]:
+                if re.search('stimulation started', data['event'][index]):
+                    stimStart.append(data['startTime'][index])
+                if re.search('stimulation stopped', data['event'][index]):
+                    stimStop.append(data['startTime'][index])
+
+        # convert lists to arrays and compute stimulation durations
+        durs = np.asarray(stimStop) - np.asarray(stimStart)
+
+        # make dataframe and save as text file
+        design = pd.DataFrame({'startTime': stimStart, 'duration': durs, 'mod' : np.ones(len(durs))})
+        np.savetxt(f'{root}/code/stimulation/{sub}/ses-01/{base}.txt', design.values, fmt='%1.2f')
+
+
 #
 #
 # # We might also want to know the distribution of inter trial intervals in order
--- code/misc/heudiconvHeuristic.py
+++ code/misc/heudiconvHeuristic.py
@@ -10,8 +10,6 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
 
 def infotodict(seqinfo):
 
-
-
     """Heuristic evaluator for determining which runs belong where
     allowed template fields - follow python string module:
     item: index within category
@@ -20,57 +18,21 @@ def infotodict(seqinfo):
     subindex: sub index within group
     """
 
-    eventStim = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-eventStim_run-00{item:01d}_cbv')
-    blockStim = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-blockStim_run-00{item:01d}_cbv')
-    slab_inv1 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_inv1')
-    slab_inv1_phs = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_inv1_phs')
-    slab_inv2 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_inv2')
-    slab_inv2_phs=create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_inv2_phs')
-    slab_div=create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_div')
-    slab_uni=create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_uni')
-    slab_t1=create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_highres-mp2rage_run-00{item:01d}_t1')
-    wholebrain_inv1 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_inv1')
-    wholebrain_inv1_phs = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_inv1_phs')
-    wholebrain_inv2 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_inv2')
-    wholebrain_inv2_phs = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_inv2_phs')
-    wholebrain_div = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_div')
-    wholebrain_uni =create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_uni')
-    wholebrain_t1 = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_t1')
-    wholebrain_uni_den = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_wholebrain-mp2rage_run-00{item:01d}_uni-den')
+    vasoMagn = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-00{item:01d}_part-mag_cbv')
+    vasoPhs = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-00{item:01d}_part-phase_cbv')
+    boldMagn = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-00{item:01d}_part-mag_bold')
+    boldPhs = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-00{item:01d}_part-phase_bold')
 
-    info = {eventStim:[],blockStim:[],slab_inv1:[],slab_inv1_phs:[],slab_inv2:[],slab_inv2_phs:[],slab_div:[],slab_uni:[],slab_t1:[],wholebrain_inv1:[], wholebrain_inv1_phs:[],wholebrain_inv2:[],wholebrain_inv2_phs:[],wholebrain_div:[],wholebrain_uni:[],wholebrain_t1:[],wholebrain_uni_den:[]}
+    info = {vasoMagn:[],vasoPhs:[],boldMagn:[],boldPhs:[]}
 
     for idx, s in enumerate(seqinfo):
-        if int(s.series_files)>=580:
-            info[eventStim].append(s.series_id)
-        if int(s.series_files)<=579 and int(s.series_files)>=100:
-            info[blockStim].append(s.series_id)
-        if ('High_Res_Slab_Anat_INV1' in s.series_description):
-            info[slab_inv1].append(s.series_id)
-        if ('High_Res_Slab_Anat_INV1_PHS' in s.series_description):
-            info[slab_inv1_phs].append(s.series_id)
-        if ('High_Res_Slab_Anat_INV2' in s.series_description):
-            info[slab_inv2].append(s.series_id)
-        if ('High_Res_Slab_Anat_INV2_PHS' in s.series_description):
-            info[slab_inv2_phs].append(s.series_id)
-        if ('High_Res_Slab_Anat_DIV_Images' in s.series_description):
-            info[slab_div].append(s.series_id)
-        if ('High_Res_Slab_Anat_UNI_Images' in s.series_description):
-            info[slab_uni].append(s.series_id)
-        if ('MP2RAGE_INV1' in s.series_description):
-            info[wholebrain_inv1].append(s.series_id)
-        if ('MP2RAGE_INV1_PHS' in s.series_description):
-            info[wholebrain_inv1_phs].append(s.series_id)
-        if ('MP2RAGE_INV2' in s.series_description):
-            info[wholebrain_inv2].append(s.series_id)
-        if ('MP2RAGE_INV2_PHS' in s.series_description):
-            info[wholebrain_inv2_phs].append(s.series_id)
-        if ('MP2RAGE_DIV_Images' in s.series_description):
-            info[wholebrain_div].append(s.series_id)
-        if ('MP2RAGE_UNI_Images' in s.series_description):
-            info[wholebrain_uni].append(s.series_id)
-        if ('MP2RAGE_T1_Images' in s.series_description):
-            info[wholebrain_t1].append(s.series_id)
-        if ('MP2RAGE_UNI-DEN' in s.series_description):
-            info[wholebrain_uni_den].append(s.series_id)
+        if s.series_files > 280:
+            if ('S00_M' in s.series_description):
+                info[vasoMagn].append(s.series_id)
+            if ('S00_P' in s.series_description):
+                info[vasoPhs].append(s.series_id)
+            if ('S01_M' in s.series_description):
+                info[boldMagn].append(s.series_id)
+            if ('S01_P' in s.series_description):
+                info[boldPhs].append(s.series_id)
     return info
