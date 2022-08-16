f5acdca0faee92ba0ba5a12a2f4e51edcfde717c
ofgulban <farukgulban@gmail.com>
2022-08-16 09:24:41 +0200

    Improve comment cosmetics

--- code/stimulation/generateDesign.py
+++ code/stimulation/generateDesign.py
@@ -16,14 +16,14 @@ folder = '/Users/sebastiandresbach/git/neurovascularCouplingVASO/code/stimulatio
 ##### Chose timings for the stimuli #####
 #########################################
 
-# set pair TR
+# Set pair TR (repetition time)
 TR = 3
-#set nr of jitters
+# Set number of jitters
 nrJitters = 6
 
-# we have 5 stimulus durations
-stimDurs =  [1, 2, 4, 12, 24] # stimulus durations in seconds
-# stimDurs =  [1, 2, 3, 4, 5] # stimulus durations in seconds
+# We have 5 stimulus durations (in seconds)
+stimDurs =  [1, 2, 4, 12, 24]
+# stimDurs =  [1, 2, 3, 4, 5]
 nrStims = len(stimDurs)
 stimDurArr = np.ones(nrJitters)
 
@@ -31,17 +31,16 @@ for i, stimDur in enumerate(stimDurs[1:]):
     for n in range(nrJitters):
         stimDurArr = np.append(stimDurArr, stimDur)
 
-# The rest periods for these stimuli are
-restDurs = [12, 14, 16, 20, 24]  # rest durations in seconds
-# restDurs = [2, 2, 2, 2, 2]  # rest durations in seconds
+# Rest periods for these stimuli (in seconds)
+restDurs = [12, 14, 16, 20, 24]
+# restDurs = [2, 2, 2, 2, 2]
 restDurArr = np.ones(nrJitters)*restDurs[0]
 
 for i, restDur in enumerate(restDurs[1:]):
     for n in range(nrJitters):
         restDurArr = np.append(restDurArr, restDur)
 
-
-# the steps between jitters are
+# Steps between jitters
 jitters = np.arange(0,nrJitters)
 jitters = jitters * (TR/nrJitters)
 jittersArr = jitters.copy()
@@ -49,7 +48,7 @@ jittersArr = jitters.copy()
 for n in range(nrStims-1):
     jittersArr = np.append(jittersArr, jitters)
 
-# make a daraframe from all the possible conditions
+# Make a dataframe from all the possible conditions
 conditions = pd.DataFrame(
     {
     'stimDur':stimDurArr,
@@ -58,24 +57,22 @@ conditions = pd.DataFrame(
     }
     )
 
-# shuffle the dataframe
+# Shuffle the dataframe
 conditions = conditions.sample(frac=1).reset_index(drop=True)
 
-# save the dataframe
+# Save the dataframe
 conditions.to_csv(
     f'{folder}/conditionTimings_TR-{TR}_jitters-{nrJitters}_{dateNow}.csv',
     index=False
     )
 
-# caculate run duration
+# Calculate run duration
 stimDurTotal = np.sum(conditions['stimDur']) # total stim duration
 restDurTotal = np.sum(conditions['restDur']) # total rest duration
 jitterDurTotal = np.sum(conditions['jitter']) # total rest duration
 
-
-# sum
+# Sum
 expDurtotal = stimDurTotal + restDurTotal + jitterDurTotal + 15 + 50 # initial and end rest
 print(f'Total experiment time: {expDurtotal} seconds')
-
 
 TRs = expDurtotal/TR
