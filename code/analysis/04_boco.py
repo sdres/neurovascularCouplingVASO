06ccad4cfe9b9a4ef4dfa808b0eedc1e39a7485c
sdres <sebastian.dresbach@gmail.com>
2022-09-15 15:16:18 +0200

    Update formatting

--- code/analysis/04_boco.py
+++ code/analysis/04_boco.py
@@ -4,28 +4,30 @@ import os
 import nibabel as nb
 import numpy as np
 import re
-
-root = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti'
-antsPath = '/Users/sebastiandresbach/ANTs/install/bin'
-afniPath = '/Users/sebastiandresbach/abin'
-layniiPath = '/Users/sebastiandresbach/git/laynii'
-
 import sys
+
 # Define current dir
 ROOT = os.getcwd()
-
 sys.path.append(os.path.abspath('./code/analysis'))
 from findTr import *
 
+ROOT = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti'
+afniPath = '/Users/sebastiandresbach/abin'
+layniiPath = '/Users/sebastiandresbach/git/laynii'
 
 # boco on mean subject mean
 for sub in ['sub-01']:
-    outFolder = f'{root}/derivatives/{sub}'
+    outFolder = f'{ROOT}/derivatives/{sub}'
 
-    tr = findTR(f'../stimulation/{sub}/ses-01/{sub}_ses-01_run-01_neurovascularCoupling.log')
+    # tr = findTR(f'../stimulation/{sub}/ses-01/{sub}_ses-01_run-01_neurovascularCoupling.log')
 
-    for modality in ['bold', 'cbv']:
-        # temporal upsampling
+    for modality in ['bold']:
+    # for modality in ['bold', 'cbv']:
+
+        # =====================================================================
+        # Temporal upsampling
+        # =====================================================================
+
         command = f'{afniPath}/3dUpsample '
         command += f'-overwrite '
         command += f'-datum short '
@@ -43,15 +45,22 @@ for sub in ['sub-01']:
             shell=True
             )
 
+        # =====================================================================
+        # Duplicate first BOLD timepoint to match timing between cbv and bold
+        # =====================================================================
+
         if modality == 'bold':
             nii = nb.load(
                 f'{outFolder}'
                 + f'/{sub}_task-stimulation_part-mag_{modality}_intemp.nii.gz'
                 )
-            data = nii.get_fdata()
-            header = nii.header
-            affine = nii.affine
 
+            # Load data
+            data = nii.get_fdata()  # Get data
+            header = nii.header  # Get header
+            affine = nii.affine  # Get affine
+
+            # Make new array
             newData = np.zeros(data.shape)
 
             for i in range(data.shape[-1]-1):
@@ -59,29 +68,43 @@ for sub in ['sub-01']:
                     newData[:,:,:,i]=data[:,:,:,i]
                 else:
                     newData[:,:,:,i]=data[:,:,:,i-1]
+
+            # Save data
             img = nb.Nifti1Image(newData.astype(int), header=header, affine=affine)
             nb.save(img, f'{outFolder}'
-                + f'/{sub}_task-stimulation_part-mag_{modality}_intemp.nii.gz'
+                + f'/{sub}_task-stimulation_part-mag_{modality}_intemp_test.nii.gz'
                 )
 
+    # ==========================================================================
+    # BOLD-correction
+    # ==========================================================================
 
-    # BOCO
-    nii1 = nb.load(f'{outFolder}/{sub}_task-stimulation_part-mag_cbv_intemp.nii.gz').get_fdata()
-    nii2 = nb.load(f'{outFolder}/{sub}_task-stimulation_part-mag_bold_intemp.nii.gz').get_fdata()
-    header = nb.load(f'{outFolder}/{sub}_task-stimulation_part-mag_cbv_intemp.nii.gz').header
-    affine = nb.load(f'{outFolder}/{sub}_task-stimulation_part-mag_cbv_intemp.nii.gz').affine
+    cbvFile = f'{outFolder}/{sub}_task-stimulation_part-mag_cbv_intemp.nii.gz'
+    boldFile = f'{outFolder}/{sub}_task-stimulation_part-mag_bold_intemp.nii.gz'
+
+    # Load data
+    nii1 = nb.load(cbvFile).get_fdata()  # Load cbv data
+    nii2 = nb.load(boldFile).get_fdata()  # Load BOLD data
+    header = nb.load(cbvFile).header  # Get header
+    affine = nb.load(cbvFile).affine  # Get affine
+
+    # Divide VASO by BOLD for actual BOCO
     new = np.divide(nii1[:,:,:,:-1], nii2[:,:,:,:-1])
 
+    # Clip range to -1.5 and 1.5. Values should be between 0 and 1 anyway.
     new[new > 1.5] = 1.5
     new[new < -1.5] = -1.5
 
+    # Save bold-corrected VASO image
     img = nb.Nifti1Image(new, header=header, affine=affine)
     nb.save(
         img, f'{outFolder}'
         + f'/{sub}_task-stimulation_part-mag_vaso_intemp.nii.gz'
         )
 
-    # calculate quality measures
+    # ==========================================================================
+    # QA
+    # ==========================================================================
     for modality in ['bold_intemp', 'vaso_intemp']:
         subprocess.run(
             f'{layniiPath}/LN_SKEW '
@@ -89,6 +112,8 @@ for sub in ['sub-01']:
             shell=True
             )
 
+    # FSL has some hickups with values between 0 and 1. Therefore, we multiply
+    # by 100.
     subprocess.run(
         f'fslmaths '
         + f'{outFolder}/{sub}_task-stimulation_part-mag_vaso_intemp.nii.gz '
