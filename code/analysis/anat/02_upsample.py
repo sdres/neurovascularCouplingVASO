d10c3efc37a38e24c7454cde097f832b1dc3ec90
sdres <sebastian.dresbach@gmail.com>
2022-10-11 16:51:01 +0200

    run for sub-05

--- code/analysis/anat/02_upsample.py
+++ code/analysis/anat/02_upsample.py
@@ -7,41 +7,17 @@ import nibabel as nb
 import glob
 
 ROOT = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti'
-SUBS = ['sub-03']
+SUBS = ['sub-05']
 
 for sub in SUBS:
-
-
     # =============================================================================
     OUTDIR = f"{ROOT}/derivatives/{sub}/anat/upsample"
-
-    files = sorted(glob.glob(f'{ROOT}/derivatives/{sub}/*/anat/*inv-2*brain.nii.gz'))
-
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+        print("Output directory is created")
     # Create this file manually in ITK-SNAP (C chape centered at occipical)
     MASK_FILE = f"{ROOT}/derivatives/{sub}/anat/{sub}_regmask.nii.gz"
 
-    # =============================================================================
-    print("MP2RAGE Step 02: Upsample.")
-
-    # Output directory
-    if not os.path.exists(OUTDIR):
-        os.makedirs(OUTDIR)
-    print("  Output directory: {}\n".format(OUTDIR))
-
-    for i, f in enumerate(files):
-        print("  Processing file {}...".format(i+1))
-        # Prepare output
-        basename, ext = f.split(os.extsep, 1)
-        basename = os.path.basename(basename)
-        out_file = os.path.join(OUTDIR, "{}_ups2X.nii.gz".format(basename))
-
-        # Prepare command
-        command1 = "c3d {} ".format(f)
-        command1 += "-interpolation Cubic "
-        command1 += "-resample 200% "
-        command1 += "-o {}".format(out_file)
-        # Execute command
-        subprocess.run(command1, shell=True)
 
     # -----------------------------------------------------------------------------
     # Also upsample registration mask
@@ -57,5 +33,33 @@ for sub in SUBS:
     command2 += "-o {}".format(out_file)
     # Execute command
     subprocess.run(command2, shell=True)
+
+    for imageType in ['inv-2']:
+        files = sorted(glob.glob(f'{ROOT}/derivatives/{sub}/*/anat/*{imageType}*brain.nii.gz'))
+
+
+        # =============================================================================
+        print(f"MP2RAGE Step 02: Upsample {imageType}.")
+
+        # Output directory
+        if not os.path.exists(OUTDIR):
+            os.makedirs(OUTDIR)
+        print("  Output directory: {}\n".format(OUTDIR))
+
+        for i, f in enumerate(files):
+            print("  Processing file {}...".format(i+1))
+            # Prepare output
+            basename, ext = f.split(os.extsep, 1)
+            basename = os.path.basename(basename)
+            out_file = os.path.join(OUTDIR, "{}_ups2X.nii.gz".format(basename))
+
+            # Prepare command
+            command1 = "c3d {} ".format(f)
+            command1 += "-interpolation Cubic "
+            command1 += "-resample 200% "
+            command1 += "-o {}".format(out_file)
+            # Execute command
+            subprocess.run(command1, shell=True)
+
 
     print('\n\nFinished.')
