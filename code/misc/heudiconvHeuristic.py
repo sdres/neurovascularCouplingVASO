2a3c32e803476d70cb4a14cda95d4fc6a97fe21d
sdres <sebastian.dresbach@gmail.com>
2022-10-11 16:49:43 +0200

    Add MEGRE

--- code/misc/heudiconvHeuristic.py
+++ code/misc/heudiconvHeuristic.py
@@ -23,10 +23,34 @@ def infotodict(seqinfo):
     boldMagn = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-0{item:01d}_part-mag_bold')
     boldPhs = create_key('sub-{subject}/{session}/func/sub-{subject}_{session}_task-stimulation_run-0{item:01d}_part-phase_bold')
 
-    info = {vasoMagn:[],vasoPhs:[],boldMagn:[],boldPhs:[]}
+    # MP2RAGE
+    mp2rageInv1Mag = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_inv-1_part-mag_run-0{item:01d}_MP2RAGE')
+    mp2rageInv1Phase = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_inv-1_part-phase_run-0{item:01d}_MP2RAGE')
+    mp2rageInv2Mag = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_inv-2_part-mag_run-0{item:01d}_MP2RAGE')
+    mp2rageInv2Phase = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_inv-2_part-phase_run-0{item:01d}_MP2RAGE')
+    mp2rageUniMag = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_uni_part-mag_run-0{item:01d}_MP2RAGE')
+    mp2rageUniPhase = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_uni_part-phase_run-0{item:01d}_MP2RAGE')
+    mp2rageT1Mag = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_t1_part-mag_run-0{item:01d}_MP2RAGE')
+    mp2rageT1Phase = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_t1_part-phase_run-0{item:01d}_MP2RAGE')
+
+
+    info = {vasoMagn: [],
+            vasoPhs:[],
+            boldMagn:[],
+            boldPhs:[],
+            # MP2RAGE
+            mp2rageInv1Mag: [],
+            mp2rageInv1Phase: [],
+            mp2rageInv2Mag: [],
+            mp2rageInv2Phase: [],
+            mp2rageUniMag: [],
+            mp2rageUniPhase: [],
+            mp2rageT1Mag: [],
+            mp2rageT1Phase: []
+            }
 
     for idx, s in enumerate(seqinfo):
-        if s.series_files > 280:
+        if s.series_files > 210:
             if ('S00_M' in s.series_description):
                 info[vasoMagn].append(s.series_id)
             if ('S00_P' in s.series_description):
@@ -35,4 +59,13 @@ def infotodict(seqinfo):
                 info[boldMagn].append(s.series_id)
             if ('S01_P' in s.series_description):
                 info[boldPhs].append(s.series_id)
+        MP2RAGE
+        if ('INV1' in s.series_description):
+            info[mp2rageInv1Mag].append(s.series_id)
+        if ('INV2' in s.series_description):
+            info[mp2rageInv2Mag].append(s.series_id)
+        if ('UNI' in s.series_description):
+            info[mp2rageUniMag].append(s.series_id)
+        if ('T1' in s.series_description):
+            info[mp2rageT1Mag].append(s.series_id)
     return info
