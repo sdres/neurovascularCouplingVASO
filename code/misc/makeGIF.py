d0f8621b8f82a6e68762036886177f9d83cf2478
sdres <sebastian.dresbach@gmail.com>
2022-11-09 14:40:13 +0100

    adapt to new figure dimensions

--- code/misc/makeGIF.py
+++ code/misc/makeGIF.py
@@ -6,7 +6,7 @@ from skimage import io
 import matplotlib.pyplot as plt
 from scipy import ndimage
 from skimage import color
-
+import numpy as np
 for modality in ['bold']:
     for stimDur in [1,2,4,12,24]:
     # for stimDur in [12,24]:
@@ -24,12 +24,12 @@ for modality in ['bold']:
             yStart = 1100
             yEnd = 2500
 
-            cropped = image[xStart:xEnd:3,yStart:yEnd:3]
+            cropped = image[xStart:xEnd,yStart:yEnd]
 
             imageio.imwrite(f'/Users/sebastiandresbach/Desktop/{modality}_{stimDur}/frame{i:02d}.png', cropped)
 
 
-    template = f'/Users/sebastiandresbach/Desktop/neurovascularCouplingFigures/fig-{modality}Activation.png'
+    template = f'/Users/sebastiandresbach/Desktop/neurovascularCouplingFigures/fig-{modality}ActivationSquare.png'
     image = io.imread(template)
     stim = '/Users/sebastiandresbach/github/neurovascularCouplingVASO/code/stimulation/visual_0.png'
     stim = io.imread(stim,as_gray=False)
@@ -38,7 +38,17 @@ for modality in ['bold']:
 
     maxFrames = len(glob.glob(f'/Users/sebastiandresbach/Desktop/{modality}_24/Scree*.png'))
 
+    yStarts = np.asarray([148, 572, 994, 1421, 1845])
+    yEnds = yStarts + 332
+
+# tmp = io.imread(f'/Users/sebastiandresbach/Desktop/{modality}_{stimDur}/frame{frame:02d}.png')
+# # tmp.shape
+# # tmp = tmp[-333:,-1174:-175,:]
+# # tmp.shape
+# # plt.imshow(tmp)
+
     for frame in range(maxFrames):
+    # for frame in range(2):
         print(f'Making frame #{frame}')
         image = io.imread(template)
 
@@ -53,8 +63,8 @@ for modality in ['bold']:
             else:
                 tmp = io.imread(f'/Users/sebastiandresbach/Desktop/{modality}_{stimDur}/frame{maxFramesStim:02d}.png')
 
-            tmp = tmp[-148:,-448:,:]
-            image[316:464,xStarts[i]:xEnds[i],:] = tmp
+            tmp = tmp[-332:,-1174:-175,:]
+            image[yStarts[i]:yEnds[i],1296:2295,:] = tmp
 
             if frame*0.785 < stimDur:
                 if frame % 2 == 0:
@@ -62,28 +72,36 @@ for modality in ['bold']:
                 if frame % 2 != 0:
                     stim = '/Users/sebastiandresbach/github/neurovascularCouplingVASO/code/stimulation/visual_1.png'
                 stim = io.imread(stim)
-                stim = stim[::10,::10]
+                stim = stim[::5,::5]
                 stim = color.gray2rgb(stim)
                 stim.shape
 
-                image[387:464,xEnds[i]-103:xEnds[i],:3] = stim
+                image[yEnds[i]-154:yEnds[i], 2090:2295,:3] = stim
 
-        imageio.imwrite(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}_{frame:02d}.png', image)
+        # imageio.imwrite(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}_{frame:02d}_large.png', image)
+
+        image = image[::3,::3,:]
+        imageio.imwrite(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}_{frame:02d}_small.png', image)
 
     # Assemble gif
-    files = sorted(glob.glob(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}*.png'))
-    # files = sorted(glob.glob(dumpFolder + '/*.png'))
-    print(f'Creating gif from {len(files)} images')
+    # for size in ['large', 'small']:
+    for size in ['small']:
+        files = sorted(glob.glob(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}*{size}.png'))
+        # files = sorted(glob.glob(dumpFolder + '/*.png'))
+        print(f'Creating gif from {len(files)} images')
 
-    images = []
-    for file in files:
-        # print(f'Adding {file}')
-        filedata = imageio.imread(file)
-        images.append(filedata)
+        images = []
+        for file in files:
+            # print(f'Adding {file}')
+            filedata = imageio.imread(file)
+            images.append(filedata)
 
-    print('Collected files')
-    print('Assembling gif')
-    imageio.mimsave(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}_movie.gif', images, duration = 1/4)
-    # print('Deleting dump directory')
-    # os.system(f'rm -r {dumpFolder}')
+        print('Collected files')
+        print('Assembling gif')
+        imageio.mimsave(f'/Users/sebastiandresbach/Desktop/gifFrames/{modality}_movie_{size}.gif', images, duration = 1/3)
+        # print('Deleting dump directory')
+        # os.system(f'rm -r {dumpFolder}')
+
+
+
     print('Done.')
