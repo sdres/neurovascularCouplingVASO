6ea25ba9d1ecb07f8a8f98e42cdc8ed7b61a8463
sdres <sebastian.dresbach@gmail.com>
2023-01-18 12:39:59 +0100

    register more data

--- code/analysis/anat/MP2RAGE/10_registerToMEGRE.py
+++ code/analysis/anat/MP2RAGE/10_registerToMEGRE.py
@@ -6,7 +6,7 @@ import numpy as np
 import nibabel as nb
 
 DATADIR = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti/derivatives'
-SUBS = ['sub-06']
+SUBS = ['sub-08']
 
 for sub in SUBS:
     # Find MEGRE session
@@ -91,7 +91,7 @@ for sub in SUBS:
     command += f'--winsorize-image-intensities [0.005,0.995] '
     command += f'--initial-moving-transform {initial} '
     command += f'--transform Rigid[0.1,3,0] '
-    command += f'--metric MI[{fixed}, {moving},1,2] '
+    command += f'--metric CC[{fixed}, {moving},1,2] '
     command += f'--convergence [60x10,1e-6,10] '
     command += f'--shrink-factors 2x1 '
     command += f'--smoothing-sigmas 1x0vox '
@@ -105,36 +105,41 @@ for sub in SUBS:
 
 
 
-    moving = f'{DATADIR}/{sub}/ses-01/anat/upsample/sub-09_rim-LH_perimeter_chunk_uncrop.nii.gz'
+    # moving = f'{DATADIR}/{sub}/ses-01/anat/upsample/sub-09_rim-LH_perimeter_chunk_uncrop.nii.gz'
+    moving = f'{DATADIR}/{sub}/ses-01/anat/upsample/{sub}_ses-01_inv-2_part-mag_run-01_MP2RAGE_N4cor_brain_crop_ups4X.nii.gz'
 
     # Prepare command to apply transform and check quality
     command = 'antsApplyTransforms '
-    # command += f'--interpolation BSpline[5] '
-    command += f'--interpolation MultiLabel '
+    command += f'--interpolation BSpline[5] '
+    # command += f'--interpolation MultiLabel '
     command += f'-d 3 -i {moving} '
     command += f'-r {fixed} '
     # command += f'-t {regFolder}/registered1_1Warp.nii.gz '
     command += f'-t {regFolder}/registered1_0GenericAffine.mat '
-    command += f'-o {regFolder}/sub-09_rim-LH_perimeter_chunk_uncrop_registered.nii.gz'
+    # command += f'-o {regFolder}/sub-09_rim-LH_perimeter_chunk_uncrop_registered.nii.gz'
+    command += f'-o {regFolder}/{sub}_ses-01_inv-2_part-mag_run-01_MP2RAGE_N4cor_brain_crop_ups4X_registered.nii.gz'
     # Run command
     subprocess.run(command,shell=True)
-    # moving = '{DATADIR}/{sub}/ses-01/anat/upsample/peri_uncrop.nii.gz'
+
+
+
+    # ==========================================================================
+    # Inverse registration to MP2RAGE
+    # ==========================================================================
 
     # moving = f'{DATADIR}/sub-06/ses-04/anat/megre/11_T2star/sub-06_ses-T2s_part-mag_MEGRE_crop_ups2X_prepped_avg_composite_decayfixed_T2s.nii.gz'
-
-    moving = f'{DATADIR}/sub-06/ses-04/anat/megre/99_Faruk/sub-05_ses-T2s_part-mag_MEGRE_crop_ups2X_prepped_avg_composite_max.nii.gz'
+    moving = f'{DATADIR}/sub-08/ses-04/anat/megre/99_Faruk/sub-08_ses-T2s_part-mag_MEGRE_crop_ups2X_prepped_avg_composite_max.nii.gz'
     basename, ext = moving.split(os.extsep, 1)
-    fixed = f'{DATADIR}/sub-06/ses-01/anat/upsample/sub-06_ses-01_uni_part-mag_run-01_MP2RAGE_N4cor_brain_crop_ups4X.nii.gz'
-    regFolder = f"{DATADIR}/sub-06/ses-01/anat/07_register_to_T2s"
+    fixed = f'{DATADIR}/sub-08/ses-01/anat/upsample/sub-08_ses-01_inv-2_part-mag_run-01_MP2RAGE_N4cor_brain_crop_ups4X.nii.gz'
+    regFolder = f"{DATADIR}/sub-08/ses-01/anat/07_register_to_T2s"
 
     # register vessels
     command = 'antsApplyTransforms '
     command += f'--interpolation BSpline[5] '
     command += f'-d 3 -i {moving} '
     command += f'-r {fixed} '
-    # command += f'-t {regFolder}/registered1_1Warp.nii.gz '
     command += f'-t [{regFolder}/registered1_0GenericAffine.mat, 1] '
-    command += f'-o {basename}_test_registered.nii.gz'
+    command += f'-o {basename}_registered.nii.gz'
     # Run command
     subprocess.run(command, shell = True)
 
