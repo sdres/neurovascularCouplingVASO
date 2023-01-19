9127e0d80bce5f37a9ca3925c791195c496c1a21
sdres <sebastian.dresbach@gmail.com>
2023-01-19 18:17:54 +0100

    Cosmetics

--- code/analysis/func/03_averageRuns.py
+++ code/analysis/func/03_averageRuns.py
@@ -1,8 +1,6 @@
 '''
 
 Averages runs with the same stimulation protocol to boost SNR.
-Runs with short ITIs are averaged within a session.
-Runs with long ITIs are averaged across sessions.
 
 '''
 
@@ -10,7 +8,6 @@ import os
 import glob
 import nibabel as nb
 import numpy as np
-import subprocess
 import sys
 
 sys.path.append('./code/misc')
@@ -18,12 +15,9 @@ sys.path.append('./code/misc')
 from computeT1w import *
 
 DATADIR = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti'
-afniPath = '/Users/sebastiandresbach/abin'
-antsPath = '/Users/sebastiandresbach/ANTs/install/bin'
 
 SUBS = ['sub-07']
 SESSIONS = ['ses-01','ses-02','ses-03','ses-04']
-# SESSIONS = ['ses-03']
 
 for sub in SUBS:
     # Create subject-directory in derivatives if it does not exist
@@ -40,11 +34,12 @@ for sub in SUBS:
         # Look for individual runs within session
         runs = sorted(glob.glob(f'{DATADIR}/{sub}/{ses}/func/{sub}_{ses}_task-stim*_run-0*_part-mag_*.nii.gz'))
 
+        # Set folder to save results
         outFolder = f'{DATADIR}/derivatives/{sub}/{ses}/func'
 
         for modality in ['cbv', 'bold']:
 
-            # find all runs of participant
+            # Find all runs of participant
             allRuns = sorted(glob.glob(f'{outFolder}/{sub}_{ses}_task-stimulation_run-0*_part-mag_{modality}_moco-reg.nii'))
             if ses == 'ses-01':
                 firstRun = sorted(glob.glob(f'{outFolder}/{sub}_{ses}_task-stimulation_run-01_part-mag_{modality}_moco.nii.gz'))
@@ -52,7 +47,7 @@ for sub in SUBS:
 
             nrRuns = len(allRuns)
 
-            # find highest number of volumes
+            # Find highest number of volumes across runs
             highstVolNr = 0
 
             for run in allRuns:
@@ -69,6 +64,7 @@ for sub in SUBS:
                 dataShape[2],
                 highstVolNr
                 )
+
             newData = np.zeros(newShape)
             divisor = np.zeros(newShape)
 
@@ -88,7 +84,7 @@ for sub in SUBS:
             header = nii.header
             affine = nii.affine
 
-            # save image
+            # Save image
             img = nb.Nifti1Image(newData, header=header, affine=affine)
             nb.save(img, f'{outFolder}/{sub}_{ses}_task-stimulation_run-avg_part-mag_{modality}.nii')
 
@@ -110,66 +106,66 @@ for sub in SUBS:
     # Average within and between sessions (long ITI)
     # ==========================================================================
 
-    for modality in ['cbv', 'bold']:
-
-        # Find runs with long ITI
-        allRuns = sorted(glob.glob(f'{DATADIR}/derivatives/{sub}/ses-*/func/{sub}_ses-*_task-stim*_run-0*_part-mag_{modality}_moco-reg.nii*'))
-        longRuns = []
-
-        for run in allRuns:
-            nii = nb.load(run)
-            nrTRs = nii.header['dim'][4]
-            # print(nrTRs)
-            if nrTRs > 250:
-                longRuns.append(run)
-
-        if any('ses-01' in s for s in longRuns):
-            firstRun = sorted(glob.glob(f'{DATADIR}/derivatives/{sub}/ses-01/func/{sub}_ses-01_task-stimulation_run-01_part-mag_{modality}_moco.nii.gz'))
-            longRuns.insert(0, firstRun[0])
-
-        nrRuns = len(longRuns)
-
-        # find highest number of volumes
-        highstVolNr = 0
-
-        for run in longRuns:
-            nii = nb.load(run)
-            header = nii.header
-            dataShape = header.get_data_shape()
-            nrVolumes = dataShape[-1]
-            if nrVolumes > highstVolNr:
-                highstVolNr = nrVolumes
-
-        newShape = (
-            dataShape[0],
-            dataShape[1],
-            dataShape[2],
-            highstVolNr
-            )
-        newData = np.zeros(newShape)
-        divisor = np.zeros(newShape)
-
-        for run in longRuns:
-            nii = nb.load(run)
-            header = nii.header
-            data = nii.get_fdata()
-            nrVolumes = data.shape[-1]
-
-
-            newData[:,:,:,:nrVolumes] += data
-            divisor[:,:,:,:nrVolumes] += 1
-
-        newData = newData/divisor
-
-        nii = nb.load(longRuns[0])
-        header = nii.header
-        affine = nii.affine
-
-        # save image
-        img = nb.Nifti1Image(newData, header=header, affine=affine)
-        nb.save(img, f'{DATADIR}/derivatives/{sub}/{sub}_ses-avg_task-stimulation_run-avg_part-mag_{modality}.nii')
-
-
+    # for modality in ['cbv', 'bold']:
+    #
+    #     # Find runs with long ITI
+    #     allRuns = sorted(glob.glob(f'{DATADIR}/derivatives/{sub}/ses-*/func/{sub}_ses-*_task-stim*_run-0*_part-mag_{modality}_moco-reg.nii*'))
+    #     longRuns = []
+    #
+    #     for run in allRuns:
+    #         nii = nb.load(run)
+    #         nrTRs = nii.header['dim'][4]
+    #         # print(nrTRs)
+    #         if nrTRs > 250:
+    #             longRuns.append(run)
+    #
+    #     if any('ses-01' in s for s in longRuns):
+    #         firstRun = sorted(glob.glob(f'{DATADIR}/derivatives/{sub}/ses-01/func/{sub}_ses-01_task-stimulation_run-01_part-mag_{modality}_moco.nii.gz'))
+    #         longRuns.insert(0, firstRun[0])
+    #
+    #     nrRuns = len(longRuns)
+    #
+    #     # find highest number of volumes
+    #     highstVolNr = 0
+    #
+    #     for run in longRuns:
+    #         nii = nb.load(run)
+    #         header = nii.header
+    #         dataShape = header.get_data_shape()
+    #         nrVolumes = dataShape[-1]
+    #         if nrVolumes > highstVolNr:
+    #             highstVolNr = nrVolumes
+    #
+    #     newShape = (
+    #         dataShape[0],
+    #         dataShape[1],
+    #         dataShape[2],
+    #         highstVolNr
+    #         )
+    #     newData = np.zeros(newShape)
+    #     divisor = np.zeros(newShape)
+    #
+    #     for run in longRuns:
+    #         nii = nb.load(run)
+    #         header = nii.header
+    #         data = nii.get_fdata()
+    #         nrVolumes = data.shape[-1]
+    #
+    #
+    #         newData[:,:,:,:nrVolumes] += data
+    #         divisor[:,:,:,:nrVolumes] += 1
+    #
+    #     newData = newData/divisor
+    #
+    #     nii = nb.load(longRuns[0])
+    #     header = nii.header
+    #     affine = nii.affine
+    #
+    #     # save image
+    #     img = nb.Nifti1Image(newData, header=header, affine=affine)
+    #     nb.save(img, f'{DATADIR}/derivatives/{sub}/{sub}_ses-avg_task-stimulation_run-avg_part-mag_{modality}.nii')
+    #
+    #
     # modalities = glob.glob(f'{DATADIR}/derivatives/{sub}/{sub}_ses-avg_task-stimulation_run-avg_part-mag_*.nii')
     #
     # t1w = computeT1w(modalities[0], modalities[1])
