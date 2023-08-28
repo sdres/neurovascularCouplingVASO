e0467ffaaae0cae0da7f125125c560437087be57
sdres <sebastian.dresbach@gmail.com>
2023-08-28 10:43:03 +0200

    Make event files for each run independently

--- code/stimulation/eventFromLogs.py
+++ code/stimulation/eventFromLogs.py
@@ -18,56 +18,62 @@ subs = ['sub-08']
 
 for sub in subs:
     # get all runs of all sessions
-    runs = sorted(glob.glob(f'{ROOT}/{sub}/ses-*/func/{sub}_ses-*_task-*_run-0*_*part-mag*_cbv.nii.gz'))
+    allRuns = sorted(glob.glob(f'{ROOT}/{sub}/ses-*/func/{sub}_ses-*_task-*_run-0*_*part-mag*_cbv.nii.gz'))
 
-    for run in runs:
-        # get basename of current run
-        base = os.path.basename(run).rsplit('.', 2)[0][:-4]
-        # see session in which it was acquired
+    # Initiate list for sessions
+    sessions = []
+    # Find all sessions
+    for run in allRuns:
+        for i in range(1, 6):  # We had a maximum of 5 sessions
+            if f'ses-0{i}' in run:
+                sessions.append(f'ses-0{i}')
 
-        for i in range(1,99):
-            if f'ses-0{i}' in base:
-                ses = f'ses-0{i}'
+    sessions = set(sessions)
 
-        log = f'code/stimulation/{sub}/{ses}/{sub}_{ses}_run-01_neurovascularCoupling.log'
-        logFile = pd.read_csv(log, usecols=[0])
+    for ses in sessions:
+        runs = sorted(glob.glob(f'{ROOT}/{sub}/{ses}/func/{sub}_{ses}_task-*_run-0*_*part-mag*_cbv.nii.gz'))
 
-        # Because the column definition will get hickups if empty colums are
-        # present, we find line with first trigger to then load the file anew,
-        # starting with that line
-        for index, row in logFile.iterrows():
-            if re.search('Keypress: 5', str(row)):
-                firstVolRow = index
-                break
+        for j, run in enumerate(runs, start=1):
+            # get basename of current run
+            base = os.path.basename(run).rsplit('.', 2)[0][:-4]
+            # see session in which it was acquired
 
-        # define column names
-        ColNames = ['startTime', 'type', 'event']
-        # load logfile again, starting with first trigger
-        # logFile = pd.read_csv(f'{ROOT}/derivatives/{sub}/{ses}/events/{base}.log', sep = '\t',skiprows=firstVolRow, names = ColNames)
+            log = f'code/stimulation/{sub}/{ses}/{sub}_{ses}_run-0{j}_neurovascularCoupling.log'
+            logFile = pd.read_csv(log, usecols=[0])
 
-        logFile = pd.read_csv(log, sep='\t', skiprows=firstVolRow, names=ColNames)
+            # Because the column definition will get hickups if empty colums are
+            # present, we find line with first trigger to then load the file anew,
+            # starting with that line
+            for index, row in logFile.iterrows():
+                if re.search('Keypress: 5', str(row)):
+                    firstVolRow = index
+                    break
 
-        # initiate lists
-        stimStart = []
-        stimStop = []
-        stimDurs = []
+            # define column names
+            ColNames = ['startTime', 'type', 'event']
+            # load logfile again, starting with first trigger
+            logFile = pd.read_csv(log, sep='\t', skiprows=firstVolRow, names=ColNames)
 
+            # initiate lists
+            stimStart = []
+            stimStop = []
+            stimDurs = []
 
-        # loop over lines and fine stimulation start and stop times
-        for index, row in logFile.iterrows():
-            if not logFile['event'][index] != logFile['event'][index]:
-                if re.search('stimulation started', logFile['event'][index]):
-                    stimStart.append(logFile['startTime'][index])
-                if re.search('stimulation stopped', logFile['event'][index]):
-                    stimStop.append(logFile['startTime'][index])
-                if re.search('stimDur', logFile['event'][index]):
-                    stimDurs.append(int(float(re.findall(r"\d+\.\d+", logFile['event'][index])[0])))
+            # loop over lines and fine stimulation start and stop times
+            for index, row in logFile.iterrows():
+                if not logFile['event'][index] != logFile['event'][index]:
+                    if re.search('stimulation started', logFile['event'][index]):
+                        stimStart.append(logFile['startTime'][index])
+                    if re.search('stimulation stopped', logFile['event'][index]):
+                        stimStop.append(logFile['startTime'][index])
+                    if re.search('stimDur', logFile['event'][index]):
+                        stimDurs.append(int(float(re.findall(r"\d+\.\d+", logFile['event'][index])[0])))
 
-        # convert lists to arrays and compute stimulation durations
-        durs = np.asarray(stimStop) - np.asarray(stimStart)
-        stim = [f'stim {stimDur}s' for stimDur in stimDurs]
+            # convert lists to arrays and compute stimulation durations
+            durs = np.asarray(stimStop) - np.asarray(stimStart)
+            stim = [f'stim {stimDur}s' for stimDur in stimDurs]
 
-        # make dataframe and save as text file
-        design = pd.DataFrame({'onset': stimStart, 'duration': durs, 'trial_type': stim})
-        for modality in ['bold', 'cbv']:
-            design.to_csv(f'{ROOT}/{sub}/{ses}/func/{base}_{modality}_events.tsv', index=False)
+            # make dataframe and save as text file
+            design = pd.DataFrame({'onset': stimStart, 'duration': durs, 'trial_type': stim})
+            for modality in ['bold', 'cbv']:
+                design.to_csv(f'{ROOT}/{sub}/{ses}/func/{base}_{modality}_events.tsv', index=False)
