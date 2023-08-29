6efdbcffa1384a3eef4dc5fc04e7292240510577
sdres <sebastian.dresbach@gmail.com>
2023-08-29 21:22:54 +0200

    Change fontsize of plots

--- code/analysis/func/07_QA.py
+++ code/analysis/func/07_QA.py
@@ -103,7 +103,8 @@ for sub in subs:
         for metric in ['tSNR']:
         # for metric in ['tSNR', 'mean', 'skew', 'kurt']:
                 files = sorted(glob.glob(f'{subFolder}/ses-*/func/{sub}_ses-*_task-stimulation_run-avg_part-mag_'
-                              f'{modality}_intemp_{metric}_registered_crop-toShpereLH.nii.gz'))
+                                         f'{modality}_intemp_{metric}_registered_crop-toShpereLH.nii.gz')
+                               )
 
             for file in files:
                 for j in range(1, 6):
@@ -146,23 +147,24 @@ for metric in data['metric'].unique():
 
     sns.kdeplot(data=tmp, x='value', hue='modality', linewidth=2, palette=palette)
 
-    plt.title(f'ROI {metric}', fontsize=24)
+    # plt.title(f'ROI {metric}', fontsize=24)
     # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-    plt.ylabel('voxel count', fontsize=24)
+    plt.ylabel('Voxel count', fontsize=24)
     plt.yticks([])
 
     ticks = np.arange(0, 61, 10)
-    plt.xticks(ticks, fontsize=14)
+    plt.xticks(ticks, fontsize=18)
     plt.xlim([0, 60])
 
-    plt.xlabel(f'{metric}', fontsize=20)
+    plt.xlabel(f'{metric}', fontsize=24)
 
     #legend hack
     old_legend = ax.legend_
     handles = old_legend.legendHandles
-    labels = [t.get_text() for t in old_legend.get_texts()]
+    # labels = [t.get_text() for t in old_legend.get_texts()]
+    labels = ['BOLD', 'VASO']
     title = old_legend.get_title().get_text()
-    ax.legend(handles, labels, loc='upper right', title='', fontsize=16)
+    ax.legend(handles, labels, loc='upper right', title='', fontsize=20)
     plt.tight_layout()
     plt.savefig(
         f'/Users/sebastiandresbach/github/neurovascularCouplingVASO/results/QA/group_{metric}.png',
@@ -198,3 +200,61 @@ for metric in data['metric'].unique():
         # plt.savefig(f'/Users/sebastiandresbach/Desktop/tSNR.png', bbox_inches='tight')
 
         plt.show()
+
+
+# Plot single sessions
+for sub in data['subject'].unique():
+    for metric in data['metric'].unique():
+        for modality in MODALITIES:
+            fig, ax = plt.subplots()
+            tmp = data.loc[(data['metric'] == metric) & (data['modality'] == modality) & (data['subject'] == sub)]
+            sns.kdeplot(data=tmp, x='value', hue='session', linewidth=2)
+
+            plt.title(f'{sub} {modality} {metric}', fontsize=24)
+            # ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+            plt.ylabel('voxel count', fontsize=24)
+            plt.yticks([])
+
+            if modality == 'vaso':
+                ticks = np.arange(0, 31, 5)
+                plt.xticks(ticks, fontsize=14)
+                plt.xlim([0, 30])
+
+            if modality == 'bold':
+                ticks = np.arange(0, 61, 10)
+                plt.xticks(ticks, fontsize=14)
+                plt.xlim([0, 60])
+
+            plt.xlabel(f'{metric}', fontsize=20)
+
+            #legend hack
+            old_legend = ax.legend_
+            handles = old_legend.legend_handles
+            labels = [t.get_text() for t in old_legend.get_texts()]
+            title = old_legend.get_title().get_text()
+            ax.legend(handles, labels, loc='upper right', title='', fontsize=16)
+            plt.tight_layout()
+            plt.savefig(f'/Users/sebastiandresbach/github/neurovascularCouplingVASO/results/QA/{sub}_{modality}_{metric}.png', bbox_inches='tight')
+
+            plt.show()
+
+# ==================================================================================================================
+# Make mean image of session 1 nulled and not nulled
+
+boldFile = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti/derivatives/sub-06/ses-01/func/sub-06_ses-01_task-stimulation_run-avg_part-mag_bold.nii'
+nulledFile = '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti/derivatives/sub-06/ses-01/func/sub-06_ses-01_task-stimulation_run-avg_part-mag_cbv.nii'
+
+nulledNii = nb.load(nulledFile)
+nulledData = nulledNii.get_fdata()
+
+nulledMean = np.mean(nulledData, axis=-1)
+img = nb.Nifti1Image(nulledMean, affine=nulledNii.affine, header=nulledNii.header)
+nb.save(img, '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti/derivatives/sub-06/ses-01/func/sub-06_ses-01_task-stimulation_run-avg_part-mag_cbv_mean.nii')
+
+nulledNii = nb.load(boldFile)
+nulledData = nulledNii.get_fdata()
+
+nulledMean = np.mean(nulledData, axis=-1)
+img = nb.Nifti1Image(nulledMean, affine=nulledNii.affine, header=nulledNii.header)
+nb.save(img, '/Users/sebastiandresbach/data/neurovascularCouplingVASO/Nifti/derivatives/sub-06/ses-01/func/sub-06_ses-01_task-stimulation_run-avg_part-mag_bold_mean.nii')
+
